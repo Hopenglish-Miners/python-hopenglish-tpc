95af142c11b20d5c48d06d05f757227bba85d7f3
Victor Rolando Sanchez Jara <vrsj@VRSJ-MacBook-Pro.local>
2017-01-03 04:23:49 +0800

    Display requests from python turning point APO

--- hello/views.py
+++ hello/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-# from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.http import JsonResponse
+# import urllib2
 import json
-# from django.forms.util import ErrorList
+import requests
+import OpenSSL
+import cryptography
 
 
 # from .models import Greeting
@@ -26,14 +28,17 @@ def index(request):
         )
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
-        errorMessages = ""
 
         if form.is_valid():
             uploadedFile = request.FILES['docfile']
+            jsonFile = None
+            errorFlag = 0
+
             try:
                 jsonFile = json.load(uploadedFile)
             except ValueError, e:
                 form.errors["Selected file is not JSON"] = ""
+                errorFlag = 1
 
             """
                 Desired fields:
@@ -42,39 +47,74 @@ def index(request):
                     postId: at least one postId for the chosen video
                     score: at least one score
             """
-            # videosSent = []
-            videosSent = {}
-            if "chosenVideo" not in jsonFile.keys():
-                form.errors["chosenVideo key is not in JSON"] = ""
-            else:
-                if len(jsonFile["chosenVideo"]) == 0:
-                    form.errors["Please send at least one video in the \"chosenVideo\" key "] = ""
+            if jsonFile is not None:
+                videosSent = {}
+                if "chosenVideo" not in jsonFile.keys():
+                    form.errors["chosenVideo key is not in JSON"] = ""
+                    errorFlag = 1
                 else:
-                    for video in jsonFile["chosenVideo"]:
-                        # videosSent.append(int(video))
-                        videosSent[video] = 0
-            if "listenScore" not in jsonFile.keys():
-                form.errors["listenScore key is not in JSON"] = ""
-            else:
-                if len(jsonFile["listenScore"]) == 0:
-                    form.errors["Please don't send an empty \"listenScore\" key "] = ""
+                    if len(jsonFile["chosenVideo"]) == 0:
+                        form.errors["Please send at least one video in the \"chosenVideo\" key "] = ""
+                        errorFlag = 1
+                    else:
+                        for video in jsonFile["chosenVideo"]:
+                            videosSent[video] = 0
+                if "listenScore" not in jsonFile.keys():
+                    form.errors["listenScore key is not in JSON"] = ""
+                    errorFlag = 1
                 else:
-                    postScores = {}
-                    for listSc in jsonFile["listenScore"]:
-                        if "postId" in listSc.keys():
-                            if "score" in listSc.keys():
-                                if listSc["postId"] in videosSent.keys():
-                                    videosSent[listSc["postId"]] = 1
+                    if len(jsonFile["listenScore"]) == 0:
+                        form.errors["Please don't send an empty \"listenScore\" key "] = ""
+                        errorFlag = 1
+                    else:
+                        postScores = {}
+                        for listSc in jsonFile["listenScore"]:
+                            if "postId" in listSc.keys():
+                                if "score" in listSc.keys():
+                                    if listSc["postId"] in videosSent.keys():
+                                        videosSent[listSc["postId"]] = 1
+                                    else:
+                                        form.errors["There is an object with postId %d that is not in the chosenVideos key" % (listSc["postId"])] = ""
+                                        errorFlag = 1
                                 else:
-                                    form.errors["There is an object with postId %d that is not in the chosenVideos key" % (listSc["postId"])] = ""
+                                    form.errors["There is an object with postId %d that has no score" % (listSc["postId"])] = ""
+                                    errorFlag = 1
                             else:
-                                form.errors["There is an object with postId %d that has no score" % (listSc["postId"])] = ""
-                        else:
-                            form.errors["There is a listenScore item without \"postId\" key in your file"] = ""
+                                form.errors["There is a listenScore item without \"postId\" key in your file"] = ""
+                                errorFlag = 1
 
-            for key, value in videosSent.iteritems():
-                if value == 0:
-                    form.errors["The video %d has no listenScore" % key] = ""
+                for key, value in videosSent.iteritems():
+                    if value == 0:
+                        form.errors["The video %d has no listenScore" % key] = ""
+                        errorFlag = 1
+
+                if errorFlag == 0:
+                    headers = {'content-type': 'application/json'}
+                    r = requests.post("https://hopenglish-tpc-interaction.herokuapp.com/getTPVideos", data=json.dumps(jsonFile), headers=headers)
+                    jsonTP = r.json()
+
+                    intVideos = []
+                    for strVid in jsonTP["videos"]:
+                        intVideos.append(str(strVid))
+                    # return HttpResponse(jsonTP["cluster"])
+                    # dictReturn = {"cluster": jsonTP["cluster"], "videos": jsonTP["videos"]}
+                    dictReturn = {"cluster": jsonTP["cluster"], "videos": intVideos}
+                    return render_to_response(
+                        'index.html',
+                        {
+                            'form': form,
+                            # 'jsonTP': json.loads(jsonTP),
+                            'dictReturn': dictReturn
+                        },
+                        context_instance=RequestContext(request)
+                    )
+                    # form.errors[r.raw] = ""
+
+                    # form.errors[json.loads(r.json())] = ""
+                    # url = 'http://wbsapi.withings.net/[service_name]?action=[action_name]&[parameters]'
+                    # serialized_data = urllib2.urlopen(url).read()
+
+                    # data = json.loads(serialized_data)
 
         else:
             form.errors["Form is not valid"] = ""
