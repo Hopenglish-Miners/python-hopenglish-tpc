3564e00a741543fd23828419b40f868c704d1e7d
Victor Rolando Sanchez Jara <vrsj@VRSJ-MacBook-Pro.local>
2017-01-03 01:54:32 +0800

    Form for file upload and initial validation

--- hello/views.py
+++ hello/views.py
@@ -1,20 +1,75 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+# from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.http import JsonResponse
+import json
+# from django.forms.util import ErrorList
 
-from .models import Greeting
+
+# from .models import Greeting
+from .forms import DocumentForm
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
+    form = DocumentForm()
+
+    # return render(request, 'index.html')
+    if request.method == 'GET':
+        return render_to_response(
+            'index.html',
+            {'form': form},
+            context_instance=RequestContext(request)
+        )
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        errorMessages = ""
+
+        if form.is_valid():
+            uploadedFile = request.FILES['docfile']
+            try:
+                jsonFile = json.load(uploadedFile)
+            except ValueError, e:
+                form.errors["Selected file is not JSON"] = ""
+
+            """
+                Desired fields:
+                chosenVideo: at least one video
+                listenScore: at least one listen score per video
+                    postId: at least one postId for the chosen video
+                    score: at least one score
+            """
+            videosSent = []
+            if "chosenVideo" not in jsonFile.keys():
+                form.errors["chosenVideo key is not in JSON"] = ""
+            else:
+                if len(jsonFile["chosenVideo"]) == 0:
+                    form.errors["Please send at least one video in the \"chosenVideo\" key "] = ""
+                else:
+                    for video in jsonFile["chosenVideo"]:
+                        videosSent.append(int(video))
+            if "listenScore" not in jsonFile.keys():
+                form.errors["listenScore key is not in JSON"] = ""
+            else:
+                if len(jsonFile["listenScore"]) == 0:
+                    form.errors["Please don't send an empty \"listenScore\" key "] = ""
+                else:
+                    
+
+            """
+            Get all videos then checl there is score for each one
+            """
 
 
-def db(request):
 
-    greeting = Greeting()
-    greeting.save()
+        else:
+            form.errors["Form is not valid"] = ""
 
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
-
+        return render_to_response(
+            'index.html',
+            {'form': form},
+            context_instance=RequestContext(request)
+        )
